2ad3eb56740e2a409a9008a33afa386ce8a8c461
Alexander Artemenko <svetlyak.40wt@gmail.com>
2012-10-24 19:19:13 +0400

    Version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='thebot-pomodoro',
-    version='0.1.0',
+    version='0.1.1',
     description='A simple productivity timer.',
     keywords='thebot pomodoro plugin',
     license = 'New BSD License',
